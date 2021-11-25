4c7ee84e35dd0d0e76d961c07398e9b3492c50d9
faizygraphics <fazitanveer2@gmail.com>
2021-11-25 13:57:19 +0500

    OOP

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int  a, b, c;
+     a=2, b=3, c=5; 
+        {
+            // (a)
+          ++a;
+          Console.WriteLine(++a);
+          Console.ReadLine();
+        }
+        {
+            // (c)
+          a++;
+          console.WriteLine(a++);
+          console.ReadLine();
+        }
+        {
+          // (d)
+          b+ = a;
+          console. WriteLine(b);
+          console.ReadLine();
+        }
+        {
+            // (e)
+          b+ = a*c;
+          console. WriteLine(b);
+          console.ReadLine();
+        }
+        {
+            // (g)
+            // (1)
+        a = a+2;
+          console. WriteLine(a);
+          console.ReadLine();
+            // (2)
+        b = 2/2;
+          console. WriteLine(b);
+          console.ReadLine();
+            // (3)
+        b = 2/2;
+          console. WriteLine(b);
+          console.ReadLine();
+        }
+        {
+            // (h);
+        b = a++ + b++;
+        console.WriteLine(b);
+        console.ReadLine();  
+
+        }
+      
+     }
+  }
+}
+
